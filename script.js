182e66964deea33b304dd139f8aba7efe07bc035
Tony Thompson <penguinprofession@icloud.com>
2018-03-25 09:35:42 -0400

    updated script file

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 
-$(`#videoForm`).submit(function(event) {
+$(`#videoForm`).submit(function (event) {
     event.preventDefault();
     const search = $(`#videoForm`).val();
     $(`#videoForm`).val(``);
     handleVideos(searchTerm);
 });
-
-function handleVideos(searchTerm) {
-    const parameters = {
-        key: `AIzaSyCrVDceP1-KwRsIVi12ODPCwS2oSHe-_7k`,
-        q: `searchTerm`,
-        part: `snippet`,
-        maxResults: 25,
-    };
-    $.getJSON(url, parameters, function(data) {
-        populateResults(data);
-    });
-}
-
-function populateResults(data) {
-    console.log(data);
-    $(`#videoResults`).empty();
-    const html= "";
-    
-}
